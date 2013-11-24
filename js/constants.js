4193a68f71c436b9244a3fb9bcfa38dfecd55e2e
Roberts Zagorskis <zagorskis.roberts@yahoo.com>
2013-11-24 13:28:54 +0000

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,6 @@
+// final/constant variables
+// not changeable
+var componentNames = new Array("Motherboard", "Storage", "Processor", "Graphics Card", "Power Supply", "RAM");
+var idNames = new Array("mobo", "hdd", "cpu", "gpu", "psu", "ram");
+var gapStr = "gap";
+var emptyStr = "-";
